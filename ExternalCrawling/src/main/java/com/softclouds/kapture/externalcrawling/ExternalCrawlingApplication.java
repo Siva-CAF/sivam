a6ad6082dfd6ff0e97f0390199af61f402f85910
Siva-CAF <sivam@ctrlaltfix.co>
2022-05-06 11:12:24 +0530

    Update ExternalCrawlingApplication.java

--- ExternalCrawling/src/main/java/com/softclouds/kapture/externalcrawling/ExternalCrawlingApplication.java
+++ ExternalCrawling/src/main/java/com/softclouds/kapture/externalcrawling/ExternalCrawlingApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * External Crawling main application
+ * 
+ * @author sivam
+ *
+ */
 @SpringBootApplication
 @EnableScheduling
 public class ExternalCrawlingApplication {
