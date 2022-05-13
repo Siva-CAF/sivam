9f71cbb513452eb63baed69907876806b9305b17
Siva-CAF <sivam@ctrlaltfix.co>
2022-05-13 13:39:06 +0530

    Added properties changes and in pom.xml file changes

---
+++ kloader/src/main/java/com/softclouds/kapture/kloader/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.softclouds.kapture.kloader;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KloaderApplication.class);
+	}
+
+}
---
+++ kwatcher/src/main/java/com/softclouds/kapture/kwatcher/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.softclouds.kapture.kwatcher;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KwatcherApplication.class);
+	}
+
+}
